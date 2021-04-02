10140229901fed14f92266cb7ca7e44f6bd09978
Elias.Yao <siran0611@gmail.com>
2021-04-02 14:17:23 +0800

    add wisckey plan.

--- src/storage/engine/mod.rs
+++ src/storage/engine/mod.rs
@@ -3,5 +3,7 @@
 //!            as is known to all that the B+Tree write method is more serious, but it has better read performance(Ologn).
 //! * SLSM : Log Structured Merged Tree given up a part of read performance be compared to B+Tree，but SLSM has very good write performance
 //!         because it's going to written in direct order.
+//! * WISCLSM : Not yet support in the short term.
 pub mod slsm;
 pub mod memory;
+pub mod wisclsm;
